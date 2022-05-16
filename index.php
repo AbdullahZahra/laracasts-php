f30bd03c424765402dd3c02b408da66c5ee76f80
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-05-16 19:19:16 +0200

    Separate PHP logic from presentation

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
+<?php
 
-    <style>
-        header {
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-    <header>
-        <h1>
-            <?= "Hello, " . htmlspecialchars($_GET['name']); ?>
-        </h1>    
-    </header>
-</body>
-</html>
+$greeting = 'Hello World';
+
+require 'index.view.php';
